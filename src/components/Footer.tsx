fe949fbf79aed4d9722f5336eb417a44637316f3
unknown <richardjr@fullstack.ph>
2023-03-14 18:19:16 +0800

    Skelital Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Box, Flex, Heading, Text, Image, Icon, Input, Button } from "@chakra-ui/react";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <Flex>
+            <Box>
+                <Heading>Latest Properties</Heading>
+                <Flex>
+                    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
+                    <Box>
+                        <Heading>Retail Store Southwest 186th Street</Heading>
+                        <Text>From $120/month</Text>
+                    </Box>
+                </Flex>
+                <Flex>
+                    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
+                    <Box>
+                        <Heading>Apartment Building with subunits</Heading>
+                        <Text>From $120/month</Text>
+                    </Box>
+                </Flex>
+            </Box>
 
-export default Footer
+            <Box>
+                <Heading>Contact US</Heading>
+                <Flex>
+                    <Icon></Icon>
+                    <Text>1-800-700-6200</Text>
+                </Flex>
+                <Flex>
+                    <Icon></Icon>
+                    <Text>info@demolink.org</Text>
+                </Flex>
+                <Flex>
+                    <Icon></Icon>
+                    <Text>3015 Grand Ave, CoconutGrove,Merrick Way, FL 12345</Text>
+                </Flex>
+            </Box>
+
+            <Box>
+                <Heading>Newsletter Signup</Heading>
+                <Text>Enter your e-mail to get the latest news of MyHome</Text>
+                <Flex>
+                    <Input placeholder='Enter your email' size='lg' type={'email'}/>
+                    <Button type={'submit'}></Button>
+                </Flex>
+                <Flex>
+                    <Icon></Icon>
+                    <Icon></Icon>
+                    <Icon></Icon>
+                    <Icon></Icon>
+                </Flex>
+            </Box>
+        </Flex>
+    );
+};
+
+export default Footer;

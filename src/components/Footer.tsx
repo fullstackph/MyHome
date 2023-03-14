5d1b5fa95bba151456de9f48541ae4821f23821c
unknown <richardjr@fullstack.ph>
2023-03-14 20:06:41 +0800

    Footer Layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,58 +1,77 @@
 import React from "react";
-import { Box, Flex, Heading, Text, Image, Icon, Input, Button } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image, Input, Button, Center, Divider } from "@chakra-ui/react";
+import { HiPhone, HiOutlineEnvelope, ImLocation2, FaFacebookF, ImGooglePlus, FaTwitter, SlSocialPintarest, GrFormAdd } from "@/utils/icons";
 
 const Footer = () => {
     return (
-        <Flex>
-            <Box>
-                <Heading>Latest Properties</Heading>
-                <Flex>
-                    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
-                    <Box>
-                        <Heading>Retail Store Southwest 186th Street</Heading>
-                        <Text>From $120/month</Text>
+        <Box>
+        <Flex bgColor={'#31323c'} p={"60px 0"} justifyContent={'center'}>
+            <Box w={'18%'}>
+                <Heading color={'#fff'} fontWeight={'600'} fontSize={'24px'} mb={'7%'}>Latest Properties</Heading>
+                <Flex alignItems={'center'} mt={'35px'}>
+                    <Image src='/assets/footer1.jpg' boxSize={{ base: '63px', md: '66px', lg: '80px' }}></Image>
+                    <Box ml={'25px'}>
+                        <Heading color={'#fff'} fontWeight={'400'} fontSize={'14px'}>Retail Store Southwest 186th Street</Heading>
+                        <Text color={'#bc986b'}>From $120/month</Text>
                     </Box>
                 </Flex>
-                <Flex>
-                    <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov'></Image>
-                    <Box>
-                        <Heading>Apartment Building with subunits</Heading>
-                        <Text>From $120/month</Text>
+                <Flex alignItems={'center'} mt={'23px'}>
+                    <Image src='/assets/footer2.jpg' boxSize={{ base: '63px', md: '66px', lg: '80px' }}></Image>
+                    <Box ml={'25px'}>
+                        <Heading color={'#fff'} fontWeight={'400'} fontSize={'14px'}>Apartment Building with subunits</Heading>
+                        <Text color={'#bc986b'}>From $120/month</Text>
                     </Box>
                 </Flex>
             </Box>
 
-            <Box>
-                <Heading>Contact US</Heading>
-                <Flex>
-                    <Icon></Icon>
-                    <Text>1-800-700-6200</Text>
+            <Center height='260px' p={'0 60px'}>
+                <Divider orientation='vertical' color={'#4a4a54'}/>
+            </Center>
+
+            <Box w={'18%'}>
+                <Heading color={'#fff'} fontWeight={'600'} fontSize={'24px'}>Contact US</Heading>
+                <Flex mt={'32px'} pb={'18px'} alignItems={'center'}>
+                    <HiPhone color={'#4a4a54'} fontWeight={'400'} fontSize={'25px'}/>
+                    <Text color={'#fff'} fontSize={'18px'} fontWeight={'400'} pl={'10px'}>1-800-700-6200</Text>
                 </Flex>
-                <Flex>
-                    <Icon></Icon>
-                    <Text>info@demolink.org</Text>
+                <Flex pb={'18px'} alignItems={'center'}>
+                    <HiOutlineEnvelope color={'#4a4a54'} fontWeight={'400'} fontSize={'25px'}/>
+                    <Text color={'#fff'} fontSize={'14px'} fontWeight={'400'} pl={'10px'}>info@demolink.org</Text>
                 </Flex>
-                <Flex>
-                    <Icon></Icon>
-                    <Text>3015 Grand Ave, CoconutGrove,Merrick Way, FL 12345</Text>
+                <Flex pb={'18px'} alignItems={'center'}>
+                    <ImLocation2 color={'#4a4a54'} fontWeight={'400'} fontSize={'25px'}/>
+                    <Text color={'#fff'} fontSize={'14px'} fontWeight={'400'} pl={'10px'}>3015 Grand Ave, CoconutGrove,Merrick Way, FL 12345</Text>
                 </Flex>
             </Box>
 
-            <Box>
-                <Heading>Newsletter Signup</Heading>
-                <Text>Enter your e-mail to get the latest news of MyHome</Text>
+            <Center height='260px' p={'0 60px'}>
+                <Divider orientation='vertical' color={'#4a4a54'}/>
+            </Center>
+
+            <Box w={'18%'}>
+                <Heading color={'#fff'} fontWeight={'600'} fontSize={'24px'}>Newsletter Signup</Heading>
+                <Text color={'#89939c'} m={'25px 0 15px 0'} fontSize={'14px'}>Enter your e-mail to get the latest news of MyHome</Text>
                 <Flex>
-                    <Input placeholder='Enter your email' size='lg' type={'email'}/>
-                    <Button type={'submit'}></Button>
+                    <Input placeholder='Your e-mail' size='lg' type={'email'} mb={'25px'} w={'75%'} borderRight={'none'} borderRadius={'unset'}/>
+                    <Button type={'submit'} backgroundColor={'#be9b6d'} color={'#fff'} borderRadius={'unset'} p={'24px'}>SUBSCRIBE</Button>
                 </Flex>
-                <Flex>
-                    <Icon></Icon>
-                    <Icon></Icon>
-                    <Icon></Icon>
-                    <Icon></Icon>
+                <Flex justifyContent={'space-between'} width={'40%'}>
+                    <FaFacebookF color={'#4a4a54'} fontSize={'20px'}/>
+                    <ImGooglePlus color={'#4a4a54'} fontSize={'24px'}/>
+                    <FaTwitter color={'#4a4a54'} fontSize={'20px'}/>
+                    <SlSocialPintarest color={'#4a4a54'} fontSize={'20px'}/>
                 </Flex>
             </Box>
         </Flex>
+
+        <Flex justifyContent={'space-around'} bg={'#2a2a34'} p={'20px 0'}>
+            <Text color={'#4a4a54'} fontSize={'14px'}>MyHome © 2023 Privacy Policy</Text>
+            <Flex alignItems={'center'}>
+                <GrFormAdd fontSize={'25px'} color={'#bc986b'}/>
+                <Text color={'#bc986b'} fontSize={'16px'}>Submit Property</Text>
+            </Flex>
+        </Flex>
+    </Box>
     );
 };
 

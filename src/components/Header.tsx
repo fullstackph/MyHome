f7429de2c81300c90a7965c49018415b3fbebf9b
Jovillene-fullstack <jovillene@fullstack.ph>
2023-03-14 18:17:14 +0800

    Header texts and icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import { HiPhone, HiOutlineEnvelope, BiLogIn } from "@/utils/icons";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Box>
+      <Box>
+        <HiPhone />
+        <Text>1-800-1234-567</Text>
+        <HiOutlineEnvelope />
+        <Text>info@demolink.org</Text>
+      </Box>
+      <Box>
+        <BiLogIn />
+        <Text>Login/Register</Text>
+      </Box>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;

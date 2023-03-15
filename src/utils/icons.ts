dc541ea74d48925e6f8903210fadf85a78b65400
richardjr-fullstack <101536515+richardjr-fullstack@users.noreply.github.com>
2023-03-15 13:06:36 +0800

    Added some Icon and re-layout the footer (#2)
    * Skelital Footer
    
    * Footer Layout
    
    * Latest update

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,6 +1,9 @@
-import { IconName } from "react-icons/hi";
 import { HiPhone } from "react-icons/hi";
 import { HiOutlineEnvelope } from "react-icons/hi2";
 import { BiLogIn } from "react-icons/bi";
+import { ImLocation2, ImGooglePlus } from 'react-icons/im'
+import { FaFacebookF, FaTwitter } from 'react-icons/fa'
+import { SlSocialPintarest } from 'react-icons/Sl'
+import { IoMdAdd } from 'react-icons/io'
 
-export { HiPhone, HiOutlineEnvelope, BiLogIn };
+export { HiPhone, HiOutlineEnvelope, BiLogIn, ImLocation2, FaFacebookF, ImGooglePlus, FaTwitter, SlSocialPintarest, IoMdAdd };

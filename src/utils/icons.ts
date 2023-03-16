2b8e6297d3aea477a76596ab8c4698a6dc8f22b8
Jovillene-fullstack <jovillene@fullstack.ph>
2023-03-16 12:42:57 +0800

    fix imports icon

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -2,15 +2,13 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { HiPhone } from "react-icons/hi";
 import { HiOutlineEnvelope } from "react-icons/hi2";
 import { GrClose } from "react-icons/gr";
-import { BsChevronDown } from "react-icons/bs";
-import { BsChevronUp } from "react-icons/bs";
+import { BsChevronDown, BsChevronUp, BsFillSquareFill } from "react-icons/bs";
 import { BiLogIn } from "react-icons/bi";
 import { ImLocation2, ImGooglePlus } from "react-icons/im";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { SlSocialPintarest } from "react-icons/Sl";
 import { IoSquareSharp } from "react-icons/io5";
 import { IoMdAdd } from "react-icons/io";
-import { BsFillSquareFill } from "react-icons/bs";
 
 export {
   HiPhone,

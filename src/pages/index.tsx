24eafc236ff32698a0a1739aadb9bf68a2eb7e96
unknown <richardjr@fullstack.ph>
2023-03-22 16:31:06 +0800

    Updated Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import { Featured } from "@/components/Featured";
 import Banner from '../components/Banner'
+import CardInfo from "@/components/CardInfo";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,9 @@ export default function Home() {
   return (
     <>
       <Layout title="My Home">
+        <Banner />
+        <CardInfo />
         <Featured />
-        <Banner/>
       </Layout>
     </>
   );

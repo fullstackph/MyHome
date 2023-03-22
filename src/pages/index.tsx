66ac61b12ace924e2706c91eb7d847551e5e562c
Jovillene-fullstack <jovillene@fullstack.ph>
2023-03-22 17:23:49 +0800

    fix layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import { Featured } from "@/components/Featured";
-import Banner from '../components/Banner'
+import Banner from "../components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
-
-
-
 
 export default function Home() {
   return (
     <>
       <Layout title="My Home">
+        <Banner />
         <Featured />
-        <Banner/>
       </Layout>
     </>
   );

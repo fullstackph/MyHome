6d4069e11ff5360613a7b9f99a1bd7b4f0d3d9b4
richardjr-fullstack <101536515+richardjr-fullstack@users.noreply.github.com>
2023-03-29 15:11:21 +0800

    Categories (#12)
    * Categories-Update
    
    * Updated banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Layout from "@/components/Layout";
 import { Featured } from "@/components/Featured";
 import Banner from '../components/Banner1'
 import CardInfo from "@/components/CardInfo";
+import Categories from "@/components/Categories";
+import What_people_say from "@/components/What_people_say";
+import Our_agents from "@/components/Our_agents";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +16,9 @@ export default function Home() {
         <Banner />
         <CardInfo />
         <Featured />
+        <Categories />
+        <What_people_say />
+        <Our_agents />
       </Layout>
     </>
   );
